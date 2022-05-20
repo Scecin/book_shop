d5cfcaf9d6a57db814b877076a951c8f9f077198
Scecin <silvia.cecin@hotmail.com>
2022-05-20 14:20:51 +0100

    first commit

---
+++ src/containers/ShopContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ShopContainer = () => {
+    return (
+        <div className="container">
+            <h1>Books</h1>
+            <hr/>
+        </div>
+    )
+}
+
+export default ShopContainer;
